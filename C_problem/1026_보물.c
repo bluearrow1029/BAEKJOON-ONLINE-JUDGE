98ea7272ec99b4c041c3651ec9d14fc987ede265
bluearrow1029 <nohyw98@gmail.com>
2019-07-29 22:20:10 +0900

    complete

--- C_problem/1026_보물.c
+++ C_problem/1026_보물.c
@@ -5,24 +5,54 @@
 
 int main()
 {
-    int *A, *B;
-    int S, i;
-    int N = 0;
-    scanf("%d",&N);
+	int *A, *B;
+	int N = 0, S = 0, i = 0, temp = 0;
+	scanf("%d", &N);
 
-    A = (int*)malloc(sizeof(int) * N);
-    B = (int*)malloc(sizeof(int) * N);
+	A = (int*)malloc(sizeof(int) * N);
+	B = (int*)malloc(sizeof(int) * N);
 
-    for (i=0; i<N; i++)
-        scanf("%d",&A[i]);
-    
-    for(i=0; i<N; i++)
-        scanf("%d",&B[i]);
-    
-        
+	for (i = 0; i<N; i++)
+		scanf("%d", &A[i]);
 
-    free(A);
-    free(B);
+	for (i = 0; i<N; i++)
+		scanf("%d", &B[i]);
+	
+	// A는 오름차순 정렬
+	for (i = 0; i < N-1; i++)
+	{
+		for (int j = 0; j < N-1-i; j++)
+		{
+			if (A[j] > A[j + 1])
+			{
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+	}
 
-    return 0;
+	// B는 내림차순 정렬
+	for (i = 0; i < N - 1; i++)
+	{
+		for (int j = 0; j < N - 1 - i; j++)
+		{
+			if (B[j] < B[j + 1])
+			{
+				temp = B[j];
+				B[j] = B[j + 1];
+				B[j + 1] = temp;
+			}
+		}
+	}
+
+	for (i = 0; i < N; i++)
+		S = S + (A[i] * B[i]);
+
+	printf("%d\n", S);
+
+	free(A);
+	free(B);
+
+	return 0;
 }
