9f784882f41b2dd85b4a4682c2c42c289ea8045e
bluearrow1029 <nohyw98@gmail.com>
2019-08-02 19:14:25 +0900

    restart

--- C_problem/1080_행렬.c
+++ C_problem/1080_행렬.c
@@ -1,30 +1,18 @@
 //문제 출처: https://www.acmicpc.net/problem/1080
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	int **A, **B;
+	char A[50][50], B[50][50];
 	int N = 0, M = 0;
 	scanf("%d %d", &N, &M);
 
-	A = (int**)malloc(sizeof(int*) * N);
-	B = (int**)malloc(sizeof(int*) * N);
+	for (int i = 0; i < N; i++)
+		scanf("%s", A[i]);
 
 	for (int i = 0; i < N; i++)
-	{
-		A[i] = (int*)malloc(sizeof(int) * M);
-		B[i] = (int*)malloc(sizeof(int) * M);
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		free(A[i]);
-		free(B[i]);
-	}
-	free(A);
-	free(B);
+		scanf("%s", B[i]);
 
 	return 0;
 }
