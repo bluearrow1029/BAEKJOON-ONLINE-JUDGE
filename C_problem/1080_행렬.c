ffcb6c721e1a81ceaa82ced908abbba74c7a6bf0
bluearrow1029 <nohyw98@gmail.com>
2019-08-04 23:14:36 +0900

    중간저장

--- C_problem/1080_행렬.c
+++ C_problem/1080_행렬.c
@@ -1,11 +1,12 @@
 //문제 출처: https://www.acmicpc.net/problem/1080
 
 #include <stdio.h>
+#define MAX 50
 
 int main()
 {
-	char A[50][50], B[50][50];
-	int N = 0, M = 0;
+	char A[MAX][MAX], B[MAX][MAX];
+	int N = 0, M = 0, count = 0;
 	scanf("%d %d", &N, &M);
 
 	for (int i = 0; i < N; i++)
@@ -13,6 +14,18 @@ int main()
 
 	for (int i = 0; i < N; i++)
 		scanf("%s", B[i]);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			if (A[i][j] != B[i][j])
+			{
+				//뒤집기
+			}
+	}
+
+	if (count >= 0)
+		printf("%d\n", count);
 
 	return 0;
 }
