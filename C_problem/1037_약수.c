bbeac40f6af3435b5fb32fb2e8eaaa20bed1d435
bluearrow1029 <nohyw98@gmail.com>
2019-08-18 23:58:35 +0900

    add sort

--- C_problem/1037_약수.c
+++ C_problem/1037_약수.c
@@ -1,10 +1,10 @@
-//문제 출처: https://www.acmicpc.net/problem/1037
+// 문제 출처: https://www.acmicpc.net/problem/1037
 
 #include <stdio.h>
 
 int main()
 {
-	int N = 0, m = 0;
+	int N = 0, m = 0, temp = 0;
 	int * a;
 
 	scanf("%d", &m);
@@ -13,7 +13,19 @@ int main()
 	for (int i = 0; i < m; i++)
 		scanf("%d", &a[i]);
 
+	// 입력된 값들을 오름차순으로 정렬
 	for (int i = 0; i < m; i++)
-		printf("%d", a[i]);
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+
 	return 0;
 }
